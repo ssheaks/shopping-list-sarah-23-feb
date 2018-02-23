48d6667c95741ed55eb89552b04f620e06c31739
ssheaks <sarah.sheaks@gmail.com>
2018-02-23 09:33:07 -0800

    add generate shopping items string and render function

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 'use strict';
 /*global $*/
 
+// `STORE` is responsible for storing the underlying data
+// that our app needs to keep track of in order to work.
+//
+// for a shopping list, our data model is pretty simple.
+// we just have an array of shopping list items. each one
+// is an object with a `name` and a `checked` property that
+// indicates if it's checked off or not.
+// we're pre-adding items to the shopping list so there's
+// something to see when the page first loads.
+
+const STORE = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
+];
+
+function generateShoppingItemsString(shoppingList) {
+  console.log 'Generating shopping list element');
+  return `
+  <li>apples</li>
+  <li>oranges</li>
+  <li>milk</li>
+  <li>bread</li>
+  `
+}
+
 function renderShoppingList() {
   // this function will be repsonsible for rendering the shopping list in
   // the DOM
   console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = '<li>apples</li>'
+  //inserts HTML into DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 
